02c637fd7187e89dcd25cc9c1e34552f864a7403
Hossein <bararianh@gmail.com>
2024-04-24 20:40:35 +0330

    Create a new Dead reaction for all enemys except for CrazyMan

--- Assets/Scripts/Enemy/EnemyTakeDamage.cs
+++ Assets/Scripts/Enemy/EnemyTakeDamage.cs
@@ -1,17 +1,22 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyTakeDamage : MonoBehaviour
 {
-   
-   
+    [Header("Dead Face")] [SerializeField] private Sprite deadEyeSprite;
+    [SerializeField] private Sprite deadMouthSprite;
+    [SerializeField] private GameObject[] eyes;
+    [SerializeField] private GameObject mouth;
+
+
     [SerializeField] private GameObject head;
     public bool isHeadCutted;
     private EnemyManager _enemyManager;
     private ToggleRagdoll _toggleRagdoll;
     private Animator _anim;
 
-   
+
     private void Start()
     {
         _enemyManager = GetComponent<EnemyManager>();
@@ -25,16 +30,23 @@ public class EnemyTakeDamage : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PlayerBullet") && !_enemyManager.isDead)
         {
+            DeadFace();
             Die();
         }
     }
 
     private void Die()
     {
-        _anim.SetTrigger("Dead");
         _enemyManager.isDead = true;
-       _enemyManager.DeActiveScripts();
+        _enemyManager.DeActiveScripts();
         _toggleRagdoll.Ragdoll(true);
+    }
+
+    private void DeadFace()
+    {
+        eyes[0].GetComponent<SpriteRenderer>().sprite = deadEyeSprite;
+        eyes[1].GetComponent<SpriteRenderer>().sprite = deadEyeSprite;
+        mouth.GetComponent<SpriteRenderer>().sprite = deadMouthSprite;
     }
 
     public void CutHead()
