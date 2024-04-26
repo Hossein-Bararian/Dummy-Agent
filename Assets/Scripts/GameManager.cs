6a1dd0c20522bb0ff74efb7c13005ea7dbe51d91
Hossein <bararianh@gmail.com>
2024-04-26 13:30:45 +0330

    Get Score animation effect was created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,20 @@ using TMPro;
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
+    
     [SerializeField] private TextMeshProUGUI txtScore;
     public static int Score;
+    private Animator _scoreAnimator;
     private void Start()
     {
+        _scoreAnimator = txtScore.GetComponent<Animator>();
         txtScore.text = "Score: 0";
     }
     public void UpdateScore(int value)
     {
         Score += value;
         txtScore.text = "Score: "+Score;
+        _scoreAnimator.Play("GetScore");
         //some effects
         //some sounds maybe !
         //some particles maybe @
