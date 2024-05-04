a79b8f9668a16432737d796b7870cf763f39a926
Hossein <bararianh@gmail.com>
2024-05-04 18:36:19 +0330

    all enemys animation diabled when player dead

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -6,27 +6,33 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public static bool IsDead;
+
     private void Awake()
     {
         IsDead = false;
     }
-    
+
     public void DeActiveScripts()
     {
         var playerScripts = GetComponents<MonoBehaviour>();
         var enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
 
-       foreach (var enemy in enemyObjects)
-       {
-           var enemyScripts = enemy.GetComponents<MonoBehaviour>();
-           foreach (var script in enemyScripts)
-           {
-               Destroy(script);
-           }
-       }
+        foreach (var enemy in enemyObjects)
+        {
+            var enemyScripts = enemy.GetComponents<MonoBehaviour>();
+            foreach (var script in enemyScripts)
+            {
+                if (script.gameObject.GetComponent<Animator>())
+                {
+                    script.gameObject.GetComponent<Animator>().enabled = false;
+                }
+                Destroy(script);
+            }
+        }
+
         foreach (var script in playerScripts)
         {
-           Destroy(script);
+            Destroy(script);
         }
     }
 }
