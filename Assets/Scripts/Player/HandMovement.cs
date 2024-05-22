786e96474c85417059e908a95620db810a57484f
Hossein <bararianh@gmail.com>
2024-05-22 19:17:01 +0330

    Add jostick assets and make a joystick for hand rotation

--- Assets/Scripts/Player/HandMovement.cs
+++ Assets/Scripts/Player/HandMovement.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 public class HandMovement : MonoBehaviour
 {
     public Transform hand;
-   
-
+    [SerializeField] private Joystick joystick;
     private void LateUpdate()
     {
-       
         MouseFollower();
     }
-
     private void MouseFollower()
     {
-        if (Camera.main != null)
+        //onWindows  -->
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var lookDir = mousePos - hand.transform.position;
+        float rotateZ = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        hand.DORotateQuaternion(Quaternion.Euler(0, 0, rotateZ + 265f), .35f);
+        //onWindows    <---
+
+        Vector2 joystickDirection = new Vector2(joystick.Horizontal, joystick.Vertical);
+        if (joystickDirection.magnitude > 0)
         {
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var lookDir = mousePos - hand.transform.position;
-            float rotateZ = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-            hand.DORotateQuaternion(Quaternion.Euler(0, 0, rotateZ + 265f), .35f);
+            float angle = Mathf.Atan2(joystickDirection.y, joystickDirection.x) * Mathf.Rad2Deg;
+            hand.DORotateQuaternion(Quaternion.Euler(0, 0, angle + 265f), .35f);
         }
     }
 }
