34706eb6cdb87dd85d804dbac249888194165a59
Hossein <bararianh@gmail.com>
2024-05-27 17:51:34 +0330

    Generate Tomb in the game

---
+++ Assets/Scripts/Generators/TombGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class TombGenerator : MonoBehaviour
+{
+    [SerializeField] private AssetReferenceGameObject spawn;
+
+    private void Start()
+    { 
+        int randomIndex = Random.Range(0,3);
+      if (randomIndex==1)
+      {
+          Addressables.InstantiateAsync(spawn, transform.position, Quaternion.identity).Completed +=
+              handle =>
+              {
+                  GameObject obj = handle.Result;
+                  obj.transform.SetParent(transform);
+              };
+      }
+    }
+}
