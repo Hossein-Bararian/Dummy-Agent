73824b90d4fa66a23899e003456c5de28bd5a046
Hossein <bararianh@gmail.com>
2024-05-28 17:21:15 +0330

    Trap bugs and Templates was fix also for Tomb , Canvas for mobile responsive fixed

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Trap : MonoBehaviour
@@ -11,13 +13,17 @@ public class Trap : MonoBehaviour
                 other.gameObject.GetComponent<PlayerTakeDamage>().Die();
             }
         }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
         if (other.gameObject.CompareTag("Enemy"))
         {
             if (other.gameObject.GetComponent<EnemyManager>())
             {
                 if (!other.gameObject.GetComponent<EnemyManager>().isDead &&
                     !other.gameObject.GetComponent<CrazyManManager>())
-                    Destroy(gameObject); 
+                    Destroy(gameObject);
             }
         }
     }
