381407d322f8751170184492ec56ffeb499160ef
Hossein <bararianh@gmail.com>
2024-06-14 17:57:59 +0330

    Trap generation fixed

--- Assets/Scripts/Generators/TrapGenerator.cs
+++ Assets/Scripts/Generators/TrapGenerator.cs
@@ -1,15 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 public class TrapGenerator : MonoBehaviour
 {
     [SerializeField] private List<GameObject> spawnList;
     private void OnEnable()
     {
-      GameObject trap =TrapPoolManager.Instance.GetTrap(spawnList[Random.Range(0,spawnList.Count)]);
-      trap.transform.position = transform.position;
-      trap.transform.rotation=Quaternion.identity;
-      trap.transform.SetParent(transform);
+        int randomIndex = Random.Range(0, 2);
+        if (randomIndex == 0)
+        {
+            GameObject trap = TrapPoolManager.Instance.GetTrap(spawnList[Random.Range(0, spawnList.Count)]);
+            trap.transform.position = transform.position;
+            trap.transform.rotation = Quaternion.identity;
+            trap.transform.SetParent(transform);
+        }
     }
 }
